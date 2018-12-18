df7506801319cedb8e510525281b608512b7bdde
build <buildautomation@syncfusion.com>
2018-12-18 19:32:01 +0530

    v16.4.0.42

--- source/jsx/rotator/default.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-ReactDOM.render(   
-	<EJ.Rotator id="sliderContent" slideWidth="100%" frameSpace={0} displayItemsCount={1} slideHeight={330} 
-	isResponsive={true} dataSource={defaultRotator} navigateSteps={1} orientation={ej.Orientation.Horizontal} 
-	showPager={true} enabled={true} showPlayButton={true} animationType="slide">
-  </EJ.Rotator>,
-	document.getElementById('rotator-default')  
-);
