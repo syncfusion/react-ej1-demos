eec2b2444055454a0a5e1d6ec421c019c4d89acf
SyncfusionInstall <buildautomation@syncfusion.com>
2020-04-01 16:34:12 +0530

    v18.1.0.42

--- app/datetimepicker/default.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-ReactDOM.render(
-    <div className="control">
-                        <EJ.DateTimePicker type="text" id="dateTime" width="100%" timeDrillDown-enabled={true} timeDrillDown-interval={5}timeDrillDown-showMeridian={true} ></EJ.DateTimePicker>
-                    </div>,
-		  document.getElementById('datetimepicker-default')
-);
