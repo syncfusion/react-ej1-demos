52b540a7d3f8a1ac8ee09db3a37b900129afe8da
SyncfusionInstall <buildautomation@syncfusion.com>
2020-05-14 10:40:11 +0530

    v18.1.0.52

--- app/pdfviewer/default.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var path =  window.baseurl + "api/PdfViewer";
-var local = 1;
-ReactDOM.render(
-React.createElement(EJ.PdfViewer, {serviceUrl: path, id: "pdfviewerControl", isResponsive: true, 
-pdfService: local}
-),
-document.getElementById('pdfviewercntl')
-);
--- build/app/datepicker/default.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-ReactDOM.render(
-   <div className="control">
-                        <EJ.DatePicker id="datepick" width="100%" ></EJ.DatePicker>
-                    </div>,
-		  document.getElementById('datepicker-default')
-);
