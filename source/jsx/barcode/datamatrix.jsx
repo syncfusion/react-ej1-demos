5191389303ef8ae7dcd15d96af3bc373f590d62b
SyncfusionInstall <buildautomation@syncfusion.com>
2020-07-07 15:18:08 +0530

    v18.2.0.44

--- source/jsx/barcode/datamatrix.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-ReactDOM.render(
-     <div className="control">
-         <EJ.Barcode id="datamatrix" text="SYNCFUSION" symbologyType="datamatrix" xDimension="7">
-         </EJ.Barcode>
-    </div>,
-document.getElementById('barcode-datamatrix')
-);
