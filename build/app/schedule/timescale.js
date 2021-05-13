4482952cb477b8e1dacacfd53bfb453b60b7ac4c
buildautomation <buildautomation@syncfusion.com>
2021-05-13 12:26:28 +0530

    v19.1.0.63

--- build/app/schedule/timescale.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var dManager = ej.DataManager($.extend(true, [], window.Default)).executeLocal(ej.Query().take(10));
-
-ReactDOM.render(
-      React.createElement(EJ.Schedule, {id: "timescaleSchedule", width: "100%", height: "525px", currentDate: new Date(2017,5,5), "timeScale-minorSlotCount": 4, "timeScale-majotSlot": 60, "appointmentSettings-dataSource": dManager, "appointmentSettings-id": "Id", "appointmentSettings-subject": "Subject", "appointmentSettings-startTime": "StartTime", "appointmentSettings-endTime": "EndTime", "appointmentSettings-description": "Description", "appointmentSettings-allDay": "AllDay", "appointmentSettings-recurrence": "Recurrence", "appointmentSettings-recurrenceRule": "RecurrenceRule"}
-      ),
-		  document.getElementById('schedule-timescale')
-);
