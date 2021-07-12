a15e53045fbbceabb061afe5ed120612731df039
BuildAutomation <buildautomation@syncfusion.com>
2021-07-12 17:43:35 +0530

    v19.2.0.44

--- app/autocomplete/grouping.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-ReactDOM.render(   
-    <EJ.Autocomplete id="grouping" width="100%" dataSource = {autocomplete_grouping} watermarkText="Select a country" filterType= { ej.filterType.Contains} 
-    itemsCount={7} highlightSearch={true} fields={auto_group_fields}>
-	</EJ.Autocomplete>,
-	document.getElementById('autocomplete-grouping')
-);
