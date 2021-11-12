827ed1de5b4913424b1d61292e05c7ef8716fa1e
SyncfusionBuild <buildautomation@syncfusion.com>
2021-11-12 16:16:42 +0530

    v19.3.0.53

--- app/autocomplete/rtl.jsx
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-ReactDOM.render(   
-    <EJ.Autocomplete id="rtl" width="100%" dataSource = {auto_complete_carList} watermarkText="Select a car" enableRTL={true}>
-    </EJ.Autocomplete>,
-    document.getElementById('autocomplete-rtl')
-);
--- build/app/autocomplete/multivalue.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-ReactDOM.render(  
-    <EJ.Autocomplete id="multivalue" width="100%" dataSource = {autocomplete_languages} watermarkText="Select skills" showPopupButton={true} 
-    multiSelectMode={ej.MultiSelectMode.Delimiter}>
-	</EJ.Autocomplete>,
-	document.getElementById('autocomplete-multivalue')
-);
