95c5f642e910551a4214a2bdb50e868664fd9685
SyncfusionBuild <buildautomation@syncfusion.com>
2022-02-01 10:18:56 +0530

    v19.4.0.48

--- build/app/autocomplete/template.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-ReactDOM.render(   
-    React.createElement(EJ.Autocomplete, {id: "template", width: "100%", dataSource: auto_template, watermarkText: "Select a country", template: "<div class=\"flag ${sprite}\"> </div><div class=\"txt\"> ${text} </div>"}
-	),
-    document.getElementById('autocomplete-template')
-);
--- build/app/barcode/code11.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-ReactDOM.render(
-     <div className="control">
-        <EJ.Barcode id="code11" text="01234567" symbologyType="code11">
-        </EJ.Barcode>
-     </div>,
-document.getElementById('barcode-code11')
-);
