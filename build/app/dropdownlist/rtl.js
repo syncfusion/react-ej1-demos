b6dfc485f92463262f0b4e5203282b167296dc9d
SyncfusionBuild <buildautomation@syncfusion.com>
2022-05-13 11:48:28 +0530

    v20.1.0.55

--- build/app/dropdownlist/rtl.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-ReactDOM.render(
-    React.createElement(EJ.DropDownList, {dataSource: SkillList, "fields-text": "skill", enableRTL: true, watermarkText: "Select a language", width: "100%"}
-    ),
-document.getElementById('dropdownlist-rtl')
-);
--- build/app/waitingpopup/default.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-ReactDOM.render(
-    React.createElement(EJ.WaitingPopup, {id: "target", appendTo: "#target", width: "100%", showOnInit: true}
-    ),
-document.getElementById('waitingpopup-default')
-);
